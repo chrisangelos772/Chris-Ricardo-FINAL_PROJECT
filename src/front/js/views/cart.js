848dac056787f689b78fc5e382589003e37e7934
chrisangelos772 <c.angelos772@gmail.com>
2021-02-19 19:58:59 +0000

    cart totals

--- src/front/js/views/cart.js
+++ src/front/js/views/cart.js
@@ -9,10 +9,15 @@ import Button from "react-bootstrap/Button";
 export const Cart = () => {
 	const { store, actions } = useContext(Context);
 	const [cart, setCart] = useState([]);
+	const [subtotal, setSubtotal] = useState(0);
+	const [total, setTotal] = useState(0);
 
-	useEffect(() => {
-		uniqueCart();
-	}, []);
+	useEffect(
+		() => {
+			uniqueCart();
+		},
+		[cart]
+	);
 
 	const uniqueCart = () => {
 		let tempCart = [...new Set(store.cart)];
@@ -33,6 +38,16 @@ export const Cart = () => {
 		}
 
 		setCart(tempCart);
+
+		calculateTotal();
+	};
+	const calculateTotal = () => {
+		let runningTotal = 0;
+		for (let i = 0; i < cart.length; i++) {
+			runningTotal += cart[i].total;
+		}
+		setSubtotal(Math.trunc(Number(runningTotal) * 100) / 100);
+		setTotal(Math.trunc(Number(runningTotal * 0.07 + runningTotal) * 100) / 100);
 	};
 
 	return (
@@ -55,6 +70,35 @@ export const Cart = () => {
 					</ListGroup.Item>
 				))}
 			</ListGroup>
+			<br />
+			<ListGroup>
+				<ListGroup.Item>
+					<Row>
+						<Col>{}</Col>
+						<Col>{}</Col>
+						<Col>Sub total</Col>
+						<Col>${subtotal}</Col>
+					</Row>
+				</ListGroup.Item>
+				<ListGroup.Item>
+					<Row>
+						<Col>{}</Col>
+						<Col>{}</Col>
+						<Col>Tax</Col>
+						<Col>7%</Col>
+					</Row>
+				</ListGroup.Item>
+				<ListGroup.Item>
+					<Row>
+						<Col>{}</Col>
+						<Col>{}</Col>
+						<Col>Total Price</Col>
+						<Col>${total}</Col>
+					</Row>
+				</ListGroup.Item>
+			</ListGroup>
+			<br />
+			<br />
 			<Button variant="danger" as={Link} to="/">
 				Go Back
 			</Button>
